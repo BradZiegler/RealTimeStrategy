ca2c69651accf759451e5f4fc787353237b4b36b
Brad Ziegler <bradleyziegler72@gmail.com>
2025-01-31 02:19:20 -0500

    Added a unit prefab and the ability for players to spawn in those units. Added the ability to select units and command only the selected unit(s) that the client has authority over. Added the functionality for players to store a list of the units that they have authority over so as to enable us to implement multiselecting in the future.

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,61 @@
+using System;
+using Mirror;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour {
+    [SerializeField] private UnityEvent onSelected = null;
+    [SerializeField] private UnityEvent onDeselected = null;
+    [SerializeField] private UnitMovement unitMovement = null;
+
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
+    public UnitMovement GetUnitMovement() {
+        return unitMovement;
+    }
+
+    #region Server
+
+    public override void OnStartServer() {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer() {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartClient() {
+        if (!isClientOnly || !isOwned) { return; }
+
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient() {
+        if (!isClientOnly || !isOwned) { return; }
+        
+        AuthorityOnUnitDespawned?.Invoke(this);
+    }
+
+    [Client]
+    public void Select() {
+        if (!isOwned) { return; }
+
+        onSelected?.Invoke();
+    }
+
+    [Client]
+    public void Deselect() {
+        if (!isOwned) { return; }
+
+        onDeselected?.Invoke();
+    }
+
+    #endregion
+}
