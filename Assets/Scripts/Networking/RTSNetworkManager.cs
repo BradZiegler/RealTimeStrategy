221067317df7a2abc67f1abeff9f57f97a92ebf0
Brad Ziegler <bradleyziegler72@gmail.com>
2025-02-07 03:16:55 -0500

    Added funtionality to keep track of remaining players when one is eleminated and declare a winner via new UI added to display the winner and include a button to leave the game.

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -1,8 +1,10 @@
 using Mirror;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RTSNetworkManager : NetworkManager {
     [SerializeField] private GameObject unitSpawnerPrefab = null;
+    [SerializeField] private GameOverHandler gameOverHandlerPrefab = null;
 
     public override void OnServerAddPlayer(NetworkConnectionToClient conn) {
         base.OnServerAddPlayer(conn);
@@ -10,5 +12,12 @@ public class RTSNetworkManager : NetworkManager {
         GameObject unitSpawnerInstance = Instantiate(unitSpawnerPrefab, conn.identity.transform.position, conn.identity.transform.rotation);
 
         NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
+
+    public override void OnServerSceneChanged(string sceneName) {
+        if (SceneManager.GetActiveScene().name.StartsWith("Scene_Map")) {
+            GameOverHandler gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
+            NetworkServer.Spawn(gameOverHandlerInstance.gameObject);
+        }
     }
 }
